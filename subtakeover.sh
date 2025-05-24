83080afd4190e53433ba67316438035be2ca3d01
Fábio Baptista <136496341+fabiozz@users.noreply.github.com>
2025-05-24 11:34:57 -0300

    Update subtakeover.sh

--- subtakeover.sh
+++ subtakeover.sh
@@ -1,4 +1,7 @@
 #!/bin/bash
+
+# verifica todos os subdominios hospedados usando "alias" baseado na name list.
+
 for palavra in $(cat lista.txt);do
 host -t cname  $palavra.$1| grep "alias for"
 done
